5419d1522e12cafb460608fcae84268295d5d083
Sander Buddendorf <s.buddendorf@nioo.knaw.nl>
2024-03-20 11:26:22 +0100

    added quit button and changed reading and writing df

--- SpeedyBat.py
+++ SpeedyBat.py
@@ -19,10 +19,11 @@ class ImageAnnotatorApp:
 
         self.social_call_counter = 0
         self.feeding_buzz_counter = 0
-        self.feeding_buzz_var = tk.BooleanVar()
         self.none_var = tk.BooleanVar()
 
-        self.load_annotations()
+        self.annotation_changes = 0
+        self.df = None
+
         self.master.geometry("800x600")
         self.create_widgets()
 
@@ -90,7 +91,14 @@ class ImageAnnotatorApp:
                                           width=self.button_width)
         self.open_file_button.grid(row=5, column=0, sticky="w")
 
-        # Image viewwer
+        # Quit button
+        self.quit_button = tk.Button(self.button_frame,
+                                          text="Quit",
+                                          command=self.quit,
+                                          width=self.button_width)
+        self.quit_button.grid(row=6, column=0, sticky="w", pady=(10,10))
+
+        # Image viewer
         self.image_label = tk.Label(self.master)
         self.image_label.place(x=self.button_frame_width, y=0, relwidth=1, relheight=1)  # Position image label
         self.master.update_idletasks()
@@ -101,13 +109,14 @@ class ImageAnnotatorApp:
         self.image_label.place(x=self.button_frame_width, y=0, width=image_label_width, relheight=1)  # Position image label
 
         # Bindings
-        self.master.bind("<s>", self.increment_social_call_key)
-        self.master.bind("<Shift-S>", self.sub_social_call_key)
-        self.master.bind("<f>", self.increment_feeding_buzz_key)
-        self.master.bind("<Shift-F>", self.sub_feeding_buzz_key)
+        self.master.bind("<s>", self.increment_social_call)
+        self.master.bind("<Shift-S>", self.sub_social_call)
+        self.master.bind("<f>", self.increment_feeding_buzz)
+        self.master.bind("<Shift-F>", self.sub_feeding_buzz)
         self.master.bind("<n>", self.toggle_none)
         self.master.bind("<space>", lambda event: self.next_image())
         self.master.bind("<r>", lambda event: self.previous_image())
+        self.master.bind("<Escape>", self.quit)
 
     def open_current_file(self):
         current_file_path = os.path.join(self.folder_path, self.image_names[self.image_index])
@@ -131,15 +140,10 @@ class ImageAnnotatorApp:
             self.show_image()
 
     def find_next_image_without_annotations(self):
-        excel_file_path = os.path.join(self.folder_path, 'annotations.xlsx')
-
-        df = pd.read_excel(excel_file_path)
-
         while self.image_index < len(self.image_names):
-            current_image_name = self.image_names[self.image_index]
-            social_call_value = df.loc[df['Image Name'] == current_image_name, 'Social Call'].iloc[0]
-            feeding_buzz_value = df.loc[df['Image Name'] == current_image_name, 'Feeding Buzz'].iloc[0]
-            none_value = df.loc[df['Image Name'] == current_image_name, 'None'].iloc[0]
+            social_call_value = self.df.at[self.image_index, 'Social Call']
+            feeding_buzz_value = self.df.at[self.image_index, 'Feeding Buzz']
+            none_value = self.df.at[self.image_index, 'None']
 
             # If annotation is found in any column, move to the next image
             if social_call_value > 0 or feeding_buzz_value > 0 or none_value == 'x':
@@ -156,34 +160,25 @@ class ImageAnnotatorApp:
 
         # Check if the annotations Excel file exists
         if os.path.exists(excel_file_path):
-            df = pd.read_excel(excel_file_path)
+            self.df = pd.read_excel(excel_file_path)
         else:
             # Create a new DataFrame if the file doesn't exist
-            df = pd.DataFrame({'Image Name': self.image_names,
-                               'Social Call': [''] * len(self.image_names),
-                               'Feeding Buzz': [''] * len(self.image_names),
-                               'None': [''] * len(self.image_names)})
+            self.df = pd.DataFrame({'Image Name': self.image_names,
+                                   'Social Call': [0] * len(self.image_names),
+                                   'Feeding Buzz': [0] * len(self.image_names),
+                                   'None': [''] * len(self.image_names)})
 
-        # Save the DataFrame to the annotations Excel file
-        df.to_excel(excel_file_path, index=False)
+        # # Save the DataFrame to the annotations Excel file
+        # df.to_excel(excel_file_path, index=False)
 
     def check_annotations(self):
-        excel_file_path = os.path.join(self.folder_path, 'annotations.xlsx')
-
-        # Check if the annotations Excel file exists
-        if not os.path.exists(excel_file_path):
-            return
-
-        df = pd.read_excel(excel_file_path)
-
         # Get the current image name
         index = self.image_index
-        current_image_name = self.image_names[index]
 
         # Check 'Social Call' and 'Feeding Buzz' columns for current image
-        social_call_value = df.at[index, 'Social Call']
-        feeding_buzz_value = df.at[index, 'Feeding Buzz']
-        none_value = df.at[index, 'None']
+        social_call_value = self.df.at[index, 'Social Call']
+        feeding_buzz_value = self.df.at[index, 'Feeding Buzz']
+        none_value = self.df.at[index, 'None']
 
         # Pre-check the checkboxes if 'x' is found in the corresponding column
         if social_call_value > 0:
@@ -276,57 +271,30 @@ class ImageAnnotatorApp:
             time.sleep(0.1)
             self.next_image()
 
-    def update_annotations(self):
-        excel_file_path = os.path.join(self.folder_path, 'annotations.xlsx')
+    def quit(self, event):
+        self.update_annotations(force=True)
+        root.destroy()
 
-        if not os.path.exists(excel_file_path):
-            raise FileNotFoundError(f"Annotations Excel file '{excel_file_path}' not found.")
+    def update_annotations(self, force=False):
+        if self.df is None:
+            return
 
-        # Number of retries
-        max_retries = 3
-        retries = 0
+        index = self.image_index
 
-        while retries < max_retries:
-            try:
-                df = pd.read_excel(excel_file_path)
+        # Update cells
+        self.df.at[index, 'Social Call'] = self.social_call_counter
+        self.df.at[index, 'Feeding Buzz'] = self.feeding_buzz_counter
+        self.df.at[index, 'None'] = 'x' if self.none_var.get() else ''
 
-                # Find the index of the current image name in the DataFrame
-                current_image_name = self.image_names[self.image_index]
-                index = self.image_index
+        # Another update done
+        self.annotation_changes += 1
+        print(self.annotation_changes)
 
-                # Update the 'Social Call' column if the checkbox is checked
-                if self.social_call_counter > 0:
-                    df.at[index, 'Social Call'] = self.social_call_counter
-                else:
-                    df.at[index, 'Social Call'] = 0
-
-                # Update the 'Feeding Buzz' column if the checkbox is checked
-                if self.feeding_buzz_counter > 0:
-                    df.at[index, 'Feeding Buzz'] = self.feeding_buzz_counter
-                else:
-                    df.at[index, 'Feeding Buzz'] = 0
-
-                # Update the 'None' column if the checkbox is checked
-                if self.none_var.get():
-                    df.at[index, 'None'] = 'x'
-                else:
-                    df.at[index, 'None'] = ''
-
-                # Save the DataFrame to the annotations Excel file
-                df.to_excel(excel_file_path, index=False)
-
-                break
-
-            except PermissionError:
-                # Catch PermissionError and wait before retrying
-                retries += 1
-                if retries < max_retries:
-                    print("PermissionError: Could not update annotations. Retrying...")
-                    time.sleep(1)  # Wait for 1 second before retrying
-                else:
-                    print("PermissionError: Could not update annotations after multiple attempts.")
-                    break
-
+        # Save the DataFrame to the annotations Excel file every 10 changes
+        if self.annotation_changes >= 10 or force:
+            excel_file_path = os.path.join(self.folder_path, 'annotations.xlsx')
+            self.df.to_excel(excel_file_path, index=False)
+            self.annotation_changes = 0  # Reset the counter after saving
 
 if __name__ == "__main__":
     root = tk.Tk()
